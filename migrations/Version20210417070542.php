943511674d0fea80283e6496c701e74ca0a20131
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-04-17 04:53:52 -0300

    infra(database): migrata from SQLITE to MYSQL

--- migrations/Version20210417070542.php
+++ migrations/Version20210417070542.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210416015006 extends AbstractMigration
+final class Version20210417070542 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210416015006 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE doctor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, crm INTEGER NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('CREATE TABLE doctor (id INT AUTO_INCREMENT NOT NULL, crm INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
